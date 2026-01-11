588acec9edd9d610f3a2fdf52e7bfae26afe7180
maybejeet <contactjeet247@gmail.com>
2026-01-11 14:03:37 +0530

    Learned structured outputs

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ import { Agent , run , tool} from "@openai/agents"
 import { z } from "zod"
 import nodemailer from "nodemailer";
 
+const WeatherEvent = z.object({
+    temperature: z.number(),
+    temperature_unit: z.enum(["°C","°F"]),
+    conditions: z.string()
+});
+
 const getWeather = tool({
     name: "Weather agent",
     description: "Returns the weather of a given city.",
     parameters: z.object({cityname : z.string().toLowerCase() }),
+     
     async execute({cityname}){
         const weatherUrl = `https://wttr.in/${cityname}?format=%C+%t`
         const response = await fetch(weatherUrl)
         return `The weather of ${cityname} is ${await response.text()}`;
     }
 })
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.GMAIL_MY_EMAIL,
-        pass: process.env.GMAIL_APP_PASS
-    }
-});
 
 const sendEmail = tool({
     name: "Send Email Agent",
@@ -30,9 +30,16 @@ const sendEmail = tool({
         text: z.string()
     }),
     async execute({to, subject, text}){
-        //console.log("To - ",to);
-        //console.log("Subject - ",subject);
-        //console.log("Body - ",text);
+        // console.log("To - ",to);
+        // console.log("Subject - ",subject);
+        // console.log("Body - ",text);
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.GMAIL_MY_EMAIL,
+                pass: process.env.GMAIL_APP_PASS
+            }
+        });
         return await transporter.sendMail({
             from: process.env.GMAIL_MY_EMAIL,
             to,
@@ -48,9 +55,13 @@ const agent = new Agent({
     name: "Pika",
     instructions: "You are an AI pikachu that solves the user query",
     model: "gpt-4o-mini",
-    tools: [getWeather, sendEmail]
+    tools: [getWeather, sendEmail],
+    outputType: WeatherEvent ,
     
 })
 
-const result = run(agent, "Email me the weather of siliguri at jeetkumarprasad69@gmail.com")
+// const result = run(agent, "What is the weather of siliguri")
+// .then( (response) => { console.log(typeof response.finalOutput.temperature)})
+
+const result = run(agent, "What is the weather of siliguri")
 .then( (response) => { console.log(response.finalOutput)})
